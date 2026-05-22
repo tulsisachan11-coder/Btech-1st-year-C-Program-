63ef42d280ca9a02a419c869add1fc1484e67b52
tulsisachan11-coder <tulsi.sachan11@gmail.com>
2026-05-22 22:18:31 +0530

    Create 01_Calculater.c

---
+++ 01_Calculater.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a, b, sum;
+    printf("Pehla number daal: ");
+    scanf("%d", &a);
+    printf("Dusra number daal: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("Total=%d", sum);
+    return 0;
+}
